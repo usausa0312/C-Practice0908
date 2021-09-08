6196b9727f80b546cb205b7c909f5f96bb9b0edb
りと あいう <rito03125@icloud.com>
2021-09-09 00:45:45 +0900

    a

--- C++Practice0908/main.cpp
+++ C++Practice0908/main.cpp
@@ -6,6 +6,7 @@ void main()
 {
     unique_ptr<man[]>men(new man[MAXMAN]);
     unique_ptr<woman>woman01(new woman);
+    
     const string s_Tag[MAXMAN] = { "‚è‚Æ","‚è‚ã‚¤‚¹‚¢","‚â‚Ü‚à‚Æ" };
 
     for (int i = 0; i < MAXMAN; ++i)
