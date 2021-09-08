24b96aedb49619290ab549a934b107e84b84c2e4
りと あいう <rito03125@icloud.com>
2021-09-09 02:28:25 +0900

    for変更

--- C++Practice0908/main.cpp
+++ C++Practice0908/main.cpp
@@ -6,12 +6,12 @@ void main()
 {
     unique_ptr<man[]>men(new man[MAXMAN]);
     unique_ptr<woman>woman01(new woman);
-    
-    const string s_Tag[MAXMAN] = { "‚è‚Æ","‚è‚ã‚¤‚¹‚¢","‚â‚Ü‚à‚Æ" };
 
-    for (int i = 0; i < MAXMAN; ++i)
+    const vector<string> s_Tag = { "‚è‚Æ","‚è‚ã‚¤‚¹‚¢","‚â‚Ü‚à‚Æ" };
+
+    for (vector<int>::size_type i = 0; i < s_Tag.size(); ++i)
     {
         men[i].SetName(s_Tag[i]);
-        cout<<men[i].GetName()<<woman01->func(&men[i])<<endl;
+        cout << men[i].GetName() << woman01->func(&men[i]) << endl;
     }
 }
